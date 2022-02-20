708cfffea1dec0e4eb7369feca99fe9646ec9106
talhar123456 <talhar123456@gmail.com>
2022-02-20 14:58:31 +0100

    add fuction

--- test.cpp
+++ test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int addFunc(int a, int b){
+    return (a + b);
+}
+
 int main()
 {
-    cout<<"Hello world";
+    
+    int sum = addFunc(2, 5);
+    cout<<sum;
+
+    return 0;
 }
